fb036e5a8df3cc83d18abf18c71ba525cf4454dd
Virtus_Chae <ybd4731@gmail.com>
2025-03-03 01:31:16 +0900

    first commit

---
+++ src/main/java/com/chaeiou/backend_producer/BackendProducerApplication.java
@@ -0,0 +1,13 @@
+package com.chaeiou.backend_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaeiou/backend_producer/BackendProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaeiou.backend_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
